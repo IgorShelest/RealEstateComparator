d36fedae1bf279ed9fc169ae22c655708b848ca3
Igor Shelest <shelest.igor@icloud.com>
2020-03-03 16:57:10 +0200

    Created 'DataAgregationService'

---
+++ RealEstateComparator/DataAgregationService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataAgregationService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
